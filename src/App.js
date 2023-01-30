4bd94f404ebe91a223b63866babb7e5265c14eac
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-30 15:37:24 -0500

    Refactored to use App function

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
-
 import { useState } from "react";
 
-export default function Counter() {
+function App() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
     setCount(count + 1);
   }
 
-  return <button onClick={handleClick}> {count} likes </button>;
-}
-
-{
-  /*
-
-function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
+        Test World!
+        <button onClick={handleClick}> {count} likes </button>
       </header>
     </div>
   );
 }
 
 export default App;
-*/
-}
