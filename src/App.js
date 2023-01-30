801cb4a0d2de1070259a66837f273c3e3e29bb68
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-30 16:17:11 -0500

    Added conditional function to correct grammar

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@ function App() {
     setCount(count + 1);
   }
 
+  function checkCount() {
+    if (count == 1) {
+      return "like";
+    } else {
+      return "likes";
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={handleClick}> {count} likes </button>
+        <button onClick={handleClick}>
+          {" "}
+          {count} {checkCount()}{" "}
+        </button>
       </header>
     </div>
   );
